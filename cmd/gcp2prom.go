f468fabdfacd787bd0d11df916f212a7465f7dad
Tye McQueen <tye.mcqueen@gmail.com>
2020-08-15 16:54:48 -0700

    Use configured prefixes (not hard-coded placeholder) to query GCP metric types.

--- cmd/gcp2prom.go
+++ cmd/gcp2prom.go
@@ -9,6 +9,7 @@ import (
 	"github.com/TyeMcQueen/tools-gcp/conn"
 	"github.com/TyeMcQueen/tools-gcp/mon"
 	"github.com/TyeMcQueen/tools-gcp/mon2prom"
+	"github.com/TyeMcQueen/tools-gcp/mon2prom/config"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -21,9 +22,11 @@ func main() {
 	proj := conn.DefaultProjectId()
 	ch, runner := mon2prom.MetricFetcher(monClient)
 	count := 0
-	for md := range monClient.StreamMetricDescs(nil, proj, "loadbalancing") {
-		if nil != mon2prom.NewVec(proj, monClient, md, ch) {
-			count++
+	for _, pref := range config.GcpPrefixes() {
+		for md := range monClient.StreamMetricDescs(nil, proj, pref) {
+			if nil != mon2prom.NewVec(proj, monClient, md, ch) {
+				count++
+			}
 		}
 	}
 	if 0 == count {
